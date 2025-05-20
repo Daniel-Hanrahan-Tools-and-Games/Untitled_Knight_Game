f7fd060d8dcd11a35b7e2938aebe1918f64030aa
Daniel-Hanrahan-Tools-and-Games <106982630+Daniel-Hanrahan-Tools-and-Games@users.noreply.github.com>
2025-05-20 12:36:37 -0400

    version 2 that adds mod support

---
+++ untitled knight game version 2/MainMenu.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class MainMenu : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	
+	private void PlayButtonPressed()
+	{
+	// starts game
+	GetTree().ChangeSceneToFile("res://gameplay.tscn");
+	}
+	
+	private void ModButtonPressed()
+	{
+	// opens mod menu
+	GetTree().ChangeSceneToFile("res://untitled knight game mod/ModGameplay.tscn");
+	}
+}
